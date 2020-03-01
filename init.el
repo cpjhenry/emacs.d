ebfd9081cebc3bbcdd195de7538f4867025af9ea
Paul Henry <cpjhenry@gmail.com>
2020-03-01 13:29:33 -0500

    Updates

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;;; Emacs configuration / pjh
+
 ;; Initialize terminal
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
@@ -5,13 +7,18 @@
 
 (setq user-mail-address "cpjhenry@gmail.com")
 
-(set-default-font "Inconsolata-18")
+(set-default-font "Inconsolata-21")
 
 (setq mac-emulate-three-button-mouse t)
 (tool-bar-mode -1) 	;; turn off tool bar
 (scroll-bar-mode -1);; turn off scrollbar
 (toggle-frame-maximized)
 ;(toggle-frame-fullscreen)
+
+(setq default-frame-alist
+	'((background-color . "ivory"))) ;; list-color-display
+(setq initial-major-mode 'text-mode)
+(setq-default major-mode 'text-mode)
 
 ;; Initialize package manager
 (eval-and-compile
@@ -28,83 +35,90 @@
 							))
 
 	(package-initialize)
-	;; i always fetch the archive contents on startup and during compilation, which is slow
-	;(package-refresh-contents)
+	;(package-refresh-contents) ;; i always fetch the archive contents on startup and during compilation
 	(unless (package-installed-p 'use-package)
 		(package-install 'use-package))
 	(require 'use-package)
 	(setf use-package-always-ensure t))
 
 ;; settings
-;(add-to-list 'load-path "~/.emacs.d/lisp/")
-;(setq site-lisp-dir (expand-file-name "lisp" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(setq custom-file (concat user-emacs-directory "/custom.el"))
+;(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(setq default-directory (concat (getenv "HOME") "/Documents"))
-(setq make-backup-files nil)
-(setq backup-directory-alist
-	`((".*" . ,temporary-file-directory)))
-(setq auto-save-default nil)
-(setq auto-save-file-name-transforms
-	`((".*" ,temporary-file-directory t)))
+;; Add directories to load-path
+(eval-and-compile
+	(mapc #'(lambda (path)
+		(add-to-list 'load-path (expand-file-name path user-emacs-directory)))
+		'("init" "site-lisp")))
+
+(setq custom-file (concat user-emacs-directory "/custom.el"))
+;(load custom-file 'noerror)
+(setq default-directory (concat (getenv "HOME") "/Documents/"))
+
 (setq ring-bell-function 'ignore)
+(setq shell-file-name "bash") ;; force full subshell
+(setq shell-command-switch "-ic")
+(setq ispell-program-name "/usr/local/bin/aspell") ;; spell checker
+(setq ispell-list-command "--list") ;; correct command
+
+;; backups
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(setq backup-directory-alist
+	'((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+	'((".*" ,temporary-file-directory t)))
 
 ;; Remove unneeded buffers
 (setq inhibit-startup-echo-area-message t)
 (setq inhibit-startup-message t) ;; 'About Emacs'
-
-;; Makes *scratch* empty.
-;; oles (http://unix.stackexchange.com/questions/19874/prevent-unwanted-buffers-from-opening)
-(setq initial-scratch-message nil)
-
-;; Removes *scratch* from buffer after the mode has been set.
-(defun remove-scratch-buffer ()
+(setq initial-scratch-message nil) ;; Makes *scratch* empty
+(defun remove-scratch-buffer () ;; Removes *scratch* from buffer after the mode has been set
 	(if (get-buffer "*scratch*")
 		(kill-buffer "*scratch*")))
-;(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
-
-;; Removes *messages* from the buffer.
-(setq-default message-log-max nil)
+	;(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+(setq-default message-log-max nil) ;; Removes *messages* from the buffer
 (kill-buffer "*Messages*")
-
-;; Removes *Completions* from buffer after you've opened a file.
-(add-hook 'minibuffer-exit-hook
+(add-hook 'minibuffer-exit-hook ;; Removes *Completions* from buffer after you've opened a file
 			'(lambda ()
-				 (let ((buffer "*Completions*"))
-					 (and (get-buffer buffer)
-								(kill-buffer buffer)))))
+				(let ((buffer "*Completions*"))
+				(and (get-buffer buffer)
+				(kill-buffer buffer)))))
 
-;; Don't show *Buffer list* when opening multiple files at the same time.
-(setq inhibit-startup-buffer-menu t)
+;; opening multiple files
+(setq inhibit-startup-buffer-menu t) ;; Don't show *Buffer list*
+(add-hook 'window-setup-hook 'delete-other-windows) ;; Show only one active window
 
-;; Show only one active window when opening multiple files at the same time.
-(add-hook 'window-setup-hook 'delete-other-windows)
+;; convenient
+(defalias 'yes-or-no-p 'y-or-n-p)
+(defalias 'rs 'replace-string)
 
-;; No more typing the whole yes or no. Just y or n will do.
-(fset 'yes-or-no-p 'y-or-n-p)
+(defalias 'lcd 'list-colors-display)
+(defalias 'ds 'desktop-save)
+(defalias 'dt 'desktop-save)
+(defalias 'dsm 'desktop-save-mode)
+
+(defalias 'elm 'emacs-lisp-mode)
+(defalias 'hm 'html-mode)
+(defalias 'jsm 'js-mode)
+(defalias 'fm 'fundamental-mode)
+(defalias 'ssm 'shell-script-mode)
+(defalias 'om 'org-mode)
 
 ;; New empty buffer
 (defun xah-new-empty-buffer ()
 	"Open a new empty buffer."
 	(interactive)
-	(let ((buf (generate-new-buffer "untitled")))
+	(let ((buf (generate-new-buffer "untitled\.txt")))
 		(switch-to-buffer buf)
 		(funcall (and initial-major-mode))
-		(setq buffer-offer-save t)))
-(setq initial-major-mode (quote text-mode))
-(global-set-key (kbd "C-n") 'xah-new-empty-buffer) ; Ctrl+n
+		(setq buffer-offer-save t)
+		(olivetti-mode)
+		))
+	(global-set-key (kbd "C-n") 'xah-new-empty-buffer) ; Ctrl+n
 
-;; force full subshell
-(setq shell-file-name "bash")
-(setq shell-command-switch "-ic")
-
-;; spell checker
-(setq ispell-program-name "/usr/local/bin/aspell")
-
-;; Initialize packages
+;;; Initialize packages
 (use-package markdown-mode
-	:ensure t
 	:commands (markdown-mode gfm-mode)
 	:mode (("README\\.md\\'" . gfm-mode)
 				 ("\\.md\\'" . markdown-mode)
@@ -114,17 +128,51 @@
 (use-package elpher) ;; gopher
 ;(require "gopher.el")
 
-(use-package visual-fill-column)
-
 ;; Org-mode stuff
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
 ;; Emacs Text mode
-;(setq-default major-mode 'text-mode)
-;(add-hook 'text-mode-hook 'turn-on-auto-fill)
-(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
-(add-hook 'visual-line-mode-hook #'visual-fill-column-mode)
-(add-hook 'text-mode-hook '(lambda() (set-fill-column 80)))
+(use-package visual-fill-column)
 (setq visual-line-fringe-indicators '(nil right-curly-arrow))
+;(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+;(add-hook 'visual-line-mode-hook #'visual-fill-column-mode)
+;(add-hook 'text-mode-hook '(lambda() (set-fill-column 80)))
+
+;; Olivetti
+(use-package olivetti
+	:bind ("C-c f" . olivetti-mode)
+	:config
+	(progn
+		(setf olivetti-body-width 80)
+		(visual-line-mode))
+	:mode ("\\.txt\\'" . olivetti-mode))
+
+;; Flyspell
+(add-hook 'text-mode-hook 'flyspell-mode)
+(eval-after-load "flyspell"
+    '(progn
+       (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
+       (define-key flyspell-mouse-map [mouse-3] #'undefined)
+       (setq flyspell-issue-message-flag nil)))
+
+;; use shift + arrow keys to switch between visible buffers
+(require 'windmove)
+(windmove-default-keybindings 'meta) ;; ‘M-left’ and ‘M-right’ to switch windows
+
+;; automatically save buffers associated with files on buffer or window switch
+(defadvice switch-to-buffer (before save-buffer-now activate)
+	(when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+	(when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+	(when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+	(when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+	(when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+	(when buffer-file-name (save-buffer)))
+;; automatically save buffers associated with files on frame (app) switch
+(add-hook 'focus-out-hook (lambda () (save-some-buffers t)))
