833fae6592b81dd3c749a68fee5598d7e4125e9e
Paul Henry <cpjhenry@gmail.com>
2020-03-24 23:59:52 -0400

    Updates

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
 
 ;; Mac command key is Super (by default)
 (setq ns-function-modifier 'hyper) ;; Mac function key is Hyper
+(setq ns-right-alternate-modifier 'alt) ;; Mac right option key is Alt
 (setq mac-emulate-three-button-mouse t)
+
 (tool-bar-mode -1) 	;; turn off tool bar
 (scroll-bar-mode -1);; turn off scrollbar
 (toggle-frame-maximized)
@@ -106,7 +108,39 @@
 		(olivetti-mode)))
 	(global-set-key (kbd "C-n") 'xah-new-empty-buffer)
 
-(defun mydired-sort ()
+(defun rename-file-and-buffer (new-name)
+	"Renames both current buffer and file it's visiting to NEW-NAME."
+	(interactive "sNew name: ")
+	(let ((name (buffer-name))
+		(filename (buffer-file-name)))
+	(if (not filename)
+		(message "Buffer '%s' is not visiting a file!" name)
+	(if (get-buffer new-name)
+		(message "A buffer named '%s' already exists!" new-name)
+	(progn
+		(rename-file filename new-name 1)
+		(rename-buffer new-name)
+		(set-visited-file-name new-name)
+		(set-buffer-modified-p nil))))))
+
+(defun move-buffer-file (dir)
+	"Moves both current buffer and file it's visiting to DIR."
+	(interactive "DNew directory: ")
+	(let* ((name (buffer-name))
+		(filename (buffer-file-name))
+		(dir
+		(if (string-match dir "\\(?:/\\|\\\\)$")
+		(substring dir 0 -1) dir))
+		(newname (concat dir "/" name)))
+	(if (not filename)
+		(message "Buffer '%s' is not visiting a file!" name)
+	(progn
+		(copy-file filename newname 1)
+		(delete-file filename)
+		(set-visited-file-name newname)
+		(set-buffer-modified-p nil) t))))
+ 
+ (defun mydired-sort ()
 	"Sort dired listings with directories first."
 	(save-excursion
 		(let (buffer-read-only)
@@ -273,3 +307,7 @@
 (global-set-key (kbd "<s-down>") 'end-of-buffer)
 (global-set-key (kbd "<s-prior>") 'backward-page) ;; s-H-up
 (global-set-key (kbd "<s-next>") 'forward-page) ;; s-H-down
+
+;; alternate keys
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
