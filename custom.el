cd927fc47fdc6fa797e7e70b62145862d487f81c
Paul Henry <cpjhenry@gmail.com>
2021-01-16 13:03:32 -0500

    Updates

--- custom.el
+++ custom.el
@@ -4,11 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-	("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(package-selected-packages
-   (quote
-	(form-feed go nswbuff wcy-swbuff elfeed wc-mode vterm-toggle visual-fill-column use-package ssh smart-mode-line olivetti markdown-mode elpher))))
+   '(wc-mode vterm-toggle visual-fill-column use-package ssh smooth-scrolling smart-mode-line org-bullets olivetti nswbuff markdown-mode go form-feed elpher elfeed)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
