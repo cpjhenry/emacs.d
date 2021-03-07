4d90efc5b04e7869445603b788322a4a22ac57e0
Paul Henry <cpjhenry@gmail.com>
2021-03-07 11:16:59 -0500

    Updates

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+ '(org-agenda-files '("~/Documents/org/daily.org" "~/Documents/org/work.org"))
  '(package-selected-packages
-   '(wc-mode vterm-toggle visual-fill-column use-package ssh smooth-scrolling smart-mode-line org-bullets olivetti nswbuff markdown-mode go form-feed elpher elfeed)))
+   '(which-key lorem-ipsum wc-mode vterm-toggle visual-fill-column use-package ssh smooth-scrolling smart-mode-line osx-browse org-bullets org olivetti nswbuff markdown-mode go form-feed elpher elfeed-web)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
