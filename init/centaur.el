b62285c69d007e1292fc02d00983b1beab45cc98
Paul Henry <cpjhenry@gmail.com>
2021-03-25 07:44:41 -0400

    Updates

---
+++ init/centaur.el
@@ -0,0 +1,10 @@
+(use-package centaur-tabs
+	:demand
+	:config
+	(centaur-tabs-mode t))
+(centaur-tabs-headline-match)
+(centaur-tabs-change-fonts "Inconsolata" 160)
+(setq centaur-tabs--buffer-show-groups t)
+(global-set-key (kbd "C-<home>")  'centaur-tabs-backward)
+(global-set-key (kbd "C-<end>") 'centaur-tabs-forward)
+
