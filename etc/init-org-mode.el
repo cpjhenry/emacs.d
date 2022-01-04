e743aadb9820e565b7fbe4129c3c7f27d8a5d87a
Paul Henry <cpjhenry@gmail.com>
2022-01-04 00:36:27 -0500

    Updates

--- etc/init-org-mode.el
+++ etc/init-org-mode.el
@@ -1,9 +1,7 @@
 ;; org-mode
 
-(setq org-todo-keywords '(
-	(sequence "TODO" "DONE")))
-(setq org-todo-keyword-faces '(
-	("INPROGRESS" . (:foreground "blue" :weight bold)))) ; add inprogress keyword
+;(setq org-todo-keywords '((sequence "TODO" "DONE")))
+;(setq org-todo-keyword-faces '(("INPROGRESS" . (:foreground "blue" :weight bold)))) ; add inprogress keyword
 
 (setq org-emphasis-alist '(
 	("*" bold)
@@ -123,40 +121,3 @@
 		(set-face-underline-p 'org-link nil)
 		(set-face-underline-p 'org-link t))
 	(iimage-mode ‘toggle))
-
-;; https://stackoverflow.com/questions/17478260/completely-hide-the-properties-drawer-in-org-mode
-(defun org-cycle-hide-drawers (state)
-  "Re-hide all drawers after a visibility state change."
-  (when (and (derived-mode-p 'org-mode)
-             (not (memq state '(overview folded contents))))
-    (save-excursion
-      (let* ((globalp (memq state '(contents all)))
-             (beg (if globalp
-                    (point-min)
-                    (point)))
-             (end (if globalp
-                    (point-max)
-                    (if (eq state 'children)
-                      (save-excursion
-                        (outline-next-heading)
-                        (point))
-                      (org-end-of-subtree t)))))
-        (goto-char beg)
-        (while (re-search-forward org-drawer-regexp end t)
-          (save-excursion
-            (beginning-of-line 1)
-            (when (looking-at org-drawer-regexp)
-              (let* ((start (1- (match-beginning 0)))
-                     (limit
-                       (save-excursion
-                         (outline-next-heading)
-                           (point)))
-                     (msg (format
-                            (concat
-                              "org-cycle-hide-drawers:  "
-                              "`:END:`"
-                              " line missing at position %s")
-                            (1+ start))))
-                (if (re-search-forward "^[ \t]*:END:" limit t)
-                  (outline-flag-region start (point-at-eol) t)
-                  (user-error msg))))))))))
