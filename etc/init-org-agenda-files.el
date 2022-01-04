eded04aeceb08ad56f973477e44f64ed716db773
Paul Henry <cpjhenry@gmail.com>
2022-01-04 00:37:01 -0500

    Updates

---
+++ etc/init-org-agenda-files.el
@@ -0,0 +1,5 @@
+;(setq org-agenda-files (list org-directory))
+;(setq org-agenda-files (list (concat org-directory "work.org")))
+;(setq org-agenda-diary-file (concat org-directory "diary.org"))
+;(setq org-mobile-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org")
+
