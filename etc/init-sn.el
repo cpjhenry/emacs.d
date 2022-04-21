df2b7719b447e31fdb37da13cc4a8de7a215e6ac
Paul Henry <cpjhenry@gmail.com>
2022-04-21 07:00:58 -0400

    Updates

--- etc/init-sn.el
+++ etc/init-sn.el
@@ -1,8 +1,9 @@
 (use-package simplenote2)
 (load "rc-sn" 'noerror)
 (simplenote2-setup)
+(setq simplenote2-directory	(concat user-emacs-directory "var/simplenote2/"))
 (setq simplenote2-markdown-notes-mode 'markdown-mode)
-(add-hook 'simplenote2-create-note-hook (lambda () (simplenote2-set-markdown) ))
+(add-hook 'simplenote2-create-note-hook (lambda () (simplenote2-set-markdown)))
 
 (defalias 'sn 'simplenote2-browse)
 (defalias 'snsm 'simplenote2-set-markdown)
