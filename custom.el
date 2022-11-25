dbeb6c178d0c5dee28e2201b07e272742a86b66f
Paul Henry <cpjhenry@gmail.com>
2022-11-25 15:16:04 -0500

    Updates

--- custom.el
+++ custom.el
@@ -4,14 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
- '(package-selected-packages
-   '(lorem-ipsum org markdown-mode olivetti org-chef org-autolist org-mobile-sync form-feed which-key wc-mode ssh smooth-scrolling simplenote2 gnugo elpher elfeed smart-mode-line unkillable-scratch persistent-scratch nswbuff use-package))
- '(recentf-auto-cleanup 'never)
- '(safe-local-variable-values
-   '((line-spacing . 0.75)
-	 (line-spacing . 0.5)
-	 (x-soft-wrap-text . false))))
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
