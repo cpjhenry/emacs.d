85139f176f1588fe799f47c52a849e45f2515b70
Paul Henry <cpjhenry@gmail.com>
2023-01-26 09:34:39 -0500

    Updates

---
+++ etc/rc/elfeed.el
@@ -0,0 +1,6 @@
+(setq elfeed-feeds '(
+	("https://endlessparentheses.com/atom.xml" tech productivity)
+	("https://www.pine64.org/feed/" tech)
+	("https://brew.sh/atom.xml" mac)
+	("http://www.reddit.com/r/emacs/.rss" emacs)
+	))
