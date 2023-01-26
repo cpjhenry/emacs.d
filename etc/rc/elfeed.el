54801590ce7c33d68be754a4137fca4053b8bdb2
Paul Henry <cpjhenry@gmail.com>
2023-01-26 09:44:58 -0500

    Remove rcs

--- etc/rc/elfeed.el
+++
@@ -1,6 +0,0 @@
-(setq elfeed-feeds '(
-	("https://endlessparentheses.com/atom.xml" tech productivity)
-	("https://www.pine64.org/feed/" tech)
-	("https://brew.sh/atom.xml" mac)
-	("http://www.reddit.com/r/emacs/.rss" emacs)
-	))
