24f8db86b1449ff3a24a58d3eef20e48187d9315
Paul Henry <cpjhenry@gmail.com>
2023-12-25 00:27:43 -0500

    Updates

--- init.el
+++ init.el
@@ -559,15 +559,18 @@
 	;; Stack Exchange
 	(use-package sx
 		:config
-		(bind-keys :prefix "C-c S"
-				   :prefix-map my-sx-map
-				   :prefix-docstring "Global keymap for SX."
-				   ("q" . sx-tab-all-questions)
-				   ("i" . sx-inbox)
-				   ("o" . sx-open-link)
-				   ("u" . sx-tab-unanswered-my-tags)
-				   ("a" . sx-ask)
-				   ("s" . sx-search)))
+		(bind-keys
+			:prefix "C-c S"
+			:prefix-map my-sx-map
+			:prefix-docstring "Global keymap for SX."
+				("q" . sx-tab-all-questions)
+				("i" . sx-inbox)
+				("o" . sx-open-link)
+				("u" . sx-tab-unanswered-my-tags)
+				("a" . sx-ask)
+				("s" . sx-search))
+		(setq
+			sx-cache-directory (concat user-emacs-directory "var/sx")) )
 	)
 
 (when *mac*
