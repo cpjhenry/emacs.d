61bfb9b09af14ac6e3119a0aa1105a20ccea5d2e
Paul Henry <cpjhenry@gmail.com>
2024-01-10 00:33:13 -0500

    Updates

--- opt/disabled-cmds.el
+++ opt/disabled-cmds.el
@@ -2,3 +2,9 @@
 (setq xbuff (generate-new-buffer "*disabled commands*"))
 (mapatoms (lambda (x) (when (get x 'disabled) (print x xbuff) ) ) )
 (switch-to-buffer xbuff)
+
+;; TODO switch to this code
+;(let ((xbuff (generate-new-buffer "*my output*")))
+;	(with-output-to-temp-buffer xbuff
+;	(print "abc")
+;	))
