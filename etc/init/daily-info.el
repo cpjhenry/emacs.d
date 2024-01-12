8c5283a8c1ff9d416473fbd8891c035a7433875b
Paul Henry <cpjhenry@gmail.com>
2024-01-12 18:27:04 -0500

    Updates

--- etc/init/daily-info.el
+++ etc/init/daily-info.el
@@ -2,6 +2,6 @@
 (defun daily-info () "Daily information."
 	(interactive)
 	(setq xbuff (generate-new-buffer "*daily-info*"))
-	(shell-command (concat user-emacs-directory ".local/" "di-mode") xbuff))
+	(shell-command "di-mode" xbuff))
 
 ; LocalWords:  di
