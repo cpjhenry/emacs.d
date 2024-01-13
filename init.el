f3b76ddaf43f6b24e58f199ffc30529251f92a64
Paul Henry <cpjhenry@gmail.com>
2024-01-13 14:11:32 -0500

    Updates

--- init.el
+++ init.el
@@ -259,6 +259,23 @@
 (midnight-mode +1)
 
 
+;; frames
+(when (featurep 'ns)
+	(defun ns-raise-emacs ()
+	"Raise Emacs."
+	(ns-do-applescript "tell application \"Emacs\" to activate"))
+
+	(defun ns-raise-emacs-with-frame (frame)
+	"Raise Emacs and select the provided frame."
+	(with-selected-frame frame
+	(when (display-graphic-p)
+	(ns-raise-emacs))))
+
+	(add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
+
+	(when (display-graphic-p) (ns-raise-emacs)))
+
+
 ;; IDO
 ;; https://www.emacswiki.org/emacs/InteractivelyDoThings
 (require 'ido)
