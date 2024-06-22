bb51dbb7775bfa903c1a819258339f9a0f9d2dec
Paul Henry <cpjhenry@gmail.com>
2024-06-22 14:14:51 -0400

    Updates

--- init.el
+++ init.el
@@ -291,14 +291,14 @@
 (when (featurep 'ns)
 	(defun ns-raise-emacs ()
 	"Raise Emacs."
-	(ns-do-applescript "tell application \"Emacs\" to activate")
-	(toggle-frame-maximized))
+		(ns-do-applescript "tell application \"Emacs\" to activate")
+		(toggle-frame-maximized))
 
 	(defun ns-raise-emacs-with-frame (frame)
 	"Raise Emacs and select the provided frame."
 	(with-selected-frame frame
-	(when (display-graphic-p)
-	(ns-raise-emacs))))
+		(when (display-graphic-p)
+			(ns-raise-emacs))))
 	(add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
 
 	(when (display-graphic-p) (ns-raise-emacs)))
