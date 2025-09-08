5bb2aa34816b66d662811d79c0eeea28fe3ceb9c
Paul Henry <cpjhenry@gmail.com>
2025-09-08 07:25:00 -0400

    Updates

--- etc/init/text.el
+++ etc/init/text.el
@@ -226,6 +226,13 @@ from point."
         (right-word)))
     (mark-word arg allow-extend)))
 
+(defun indent-whole-buffer ()
+  "Indent the entire buffer without affecting point or mark."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (indent-region (point-min) (point-max)))))
+
 ;; eww functions
 
 (defun eww-reddit-redirect (url)
@@ -238,10 +245,3 @@ from point."
 (defun align-equals (begin end)
 	(interactive "r")
 	(align-regexp begin end "\\(\\s-*\\)=" 1 1))
-
-(defun indent-whole-buffer ()
-  "Indent the entire buffer without affecting point or mark."
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (indent-region (point-min) (point-max)))))
